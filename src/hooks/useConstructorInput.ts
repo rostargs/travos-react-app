a633339894e6a547c300c8e55609b12f854027a2
rostargs <115803196+rostargs@users.noreply.github.com>
2023-12-24 20:39:53 +0200

    fix trim method

--- src/hooks/useConstructorInput.ts
+++ src/hooks/useConstructorInput.ts
@@ -32,7 +32,7 @@ export const useConstructorInput = <T extends { [key: string]: string }>(
       ...prev,
       [name]: {
         ...prev[name],
-        value: value.trim(),
+        value: value,
       },
     }));
   };
@@ -44,13 +44,13 @@ export const useConstructorInput = <T extends { [key: string]: string }>(
 
   const onChangeQuestionInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     handleChangeInput(event);
-    dispatch(saveQuestionValue({ id: id, value: event.target.value.trim() }));
+    dispatch(saveQuestionValue({ id: id, value: event.target.value }));
     dispatch(setErrorValue({ questionID: id, value: checkValidation(event), type: "question" }));
   };
 
   const onChangeSimpleAnswerInput = (event: React.ChangeEvent<HTMLInputElement>) => {
     handleChangeInput(event);
-    dispatch(saveSimpleAnswerValue({ id, value: event.target.value.trim() }));
+    dispatch(saveSimpleAnswerValue({ id, value: event.target.value }));
     dispatch(setErrorValue({ questionID: id, value: checkValidation(event), type: "answers" }));
   };
 
